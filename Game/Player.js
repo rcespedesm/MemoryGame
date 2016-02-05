43bc6fdf7604411ba4aa26a5a264a113cb947ce9
Rodrigo Saul Cespedes Murillo <rsc.murillo@gmail.com>
2016-02-04 23:46:07 -0400

    Player Class: Attributes

---
+++ Game/Player.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2/4/2016.
+ */
+
+var Player = function(name, id){
+    this.id = id;
+    this.name = name;
+    this.wrongAttempts = 0;
+    this.goodAttempts = 0;
+    this.finalScore = 0;
+};
+
+
